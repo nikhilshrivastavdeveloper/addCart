e28920cb14c199844667d7ef7ea719185cef8683
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-02-21 02:10:31 +0530

    edit file path

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ function relocate(ele) {
         // http://127.0.0.1:3000/index.html
         //https://nikhilshrivastavdeveloper.github.io//addCart//index.html
         let origin = window.location.origin;
-        let pathname = window.location.pathname;
-        // window.location.href = `${origin}/productdesc.html?q=${ele.parentNode.id}`;
-        window.location.href = `${origin}/../../productdesc.html?q=${ele.parentNode.id}`
+        let pathname = window.location.pathname.slice(0,7);
+        window.location.href = `${origin}${pathname}/productdesc.html?q=${ele.parentNode.id}`;
     }
 }
 
