59a9928e1b2bbae95e985133487e7203449ccdcb
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-02-21 01:49:59 +0530

    just reconstruct href

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import { mainBox } from "./showproduct.js";
 function relocate(ele) {
     if (ele.tagName === "BUTTON") {
         // http://127.0.0.1:3000/index.html
+        //https://nikhilshrivastavdeveloper.github.io//addCart//index.html
         let origin = window.location.origin;
         let pathname = window.location.pathname;
-        window.location.href = `${origin}${pathname}/productdesc.html?q=${ele.parentNode.id}`;
+        window.location.href = `${origin}/productdesc.html?q=${ele.parentNode.id}`;
     }
 }
 
