a21d30bc5eff75290045184290c86ef901071bba
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-02-21 21:17:10 +0530

    redirect url generate dynamically

--- src/cart.js
+++ src/cart.js
@@ -118,7 +118,7 @@ cardBox.addEventListener("click", (e) => {
         }
         else {
             origin = window.location.origin;
-            window.location.href = `${origin}${pathname}/productdesc.html?q=${id}&quantity=${quantity}`;
+            window.location.href = `${origin}/productdesc.html?q=${id}&quantity=${quantity}`;
         }
     }
 })
