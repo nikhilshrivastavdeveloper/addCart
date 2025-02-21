74db5b0203415c8fa1835fe70ae0a6e5bf0d54ac
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-02-21 20:37:07 +0530

    redirect url generate dynamically

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import { mainBox } from "./showproduct.js";
 function relocate(ele) {
     if (ele.tagName === "BUTTON") {
         // http://127.0.0.1:3000/index.html
-        //https://nikhilshrivastavdeveloper.github.io//addCart//index.html
-        let origin = window.location.origin;
-        let pathname = window.location.pathname.slice(0,8);
-        window.location.href = `${origin}${pathname}/productdesc.html?q=${ele.parentNode.id}`;
+        //https://nikhilshrivastavdeveloper.github.io/addCart/index.html
+        
+        let origin;
+        if (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
+            origin = window.location.origin;
+            window.location.href = `${origin}/productdesc.html?q=${ele.parentNode.id}`;
+        }
+        else {
+            origin = window.location.origin;
+            window.location.href = `${origin}/addCart/productdesc.html?q=${ele.parentNode.id}`;
+        }
+        //above if condition check environment than construct url dynamically
     }
 }
 
